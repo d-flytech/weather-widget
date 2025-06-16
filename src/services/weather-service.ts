8a03a32728504b8b15061d7297957ee6f712ac82
d-flytech <desirevanbrummen@hotmail.com>
2025-06-16 14:29:51 +0200

    Added new directory structure and code for weather widget

--- src/services/weather-service.ts
+++ src/services/weather-service.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { WeatherData } from "./types";
+import { WeatherData } from "../types/types";
 
 const API_URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min&timezone=Europe/Berlin";
 
