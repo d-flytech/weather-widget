736ba47ce0f7e8861f8e130b68785925467b5020
d-flytech <desirevanbrummen@hotmail.com>
2025-06-20 20:11:46 +0200

    added weather type codes within api and code

--- src/services/weather-service.ts
+++ src/services/weather-service.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { WeatherData } from "../types/types";
 
-const API_URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min&timezone=Europe/Berlin";
+const API_URL =
+  "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=Europe/Berlin";
 
 export async function getWeather(): Promise<WeatherData> {
 try{
--- src/types/types.ts
+++ src/types/types.ts
@@ -2,4 +2,5 @@ export interface WeatherData {
     time: string[];
     temperature_2m_max: number[];
     temperature_2m_min: number[];
+    weathercode: number[];
 };
